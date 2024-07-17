1b10c4d89ea46ae41d6239f3fc7bfce5effc24dd
Adriana Rangel <adri.rangel6@gmail.com>
2024-07-16 23:31:09 -0700

    update errors

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,31 +2,10 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar.js";
-import Home from "./Pages/Home";
+import Home from "./pages/Home";
 import News from "./pages/News";
 
 function App() {
-	return (
-		<div>
-			<form action="" id="login" method="post">
-				<h1>Login</h1>
-				<p className="item">
-					<label for="email"> Email </label>
-					<input type="email" name="email" id="email" />
-				</p>
-				<p className="item">
-					<label for="password"> Password </label>
-					<input type="password" name="password" id="password" />
-				</p>
-				<p className="item">
-					<input type="submit" value="Login" />
-				</p>
-			</form>
-		</div>
-	)
-}
-
-function App => {
 	return (
 		<Router>
 			<Navbar />
